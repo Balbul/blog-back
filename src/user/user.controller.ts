6ae7433880cdaa59d12142c51b6c0df4d2d9d4f1
LANCIEN <lancien.louis@gmail.com>
2022-10-01 00:22:15 +0200

    preparation du travail

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { Controller, Post, Body, Patch, Delete, Get } from '@nestjs/common';
 import { UserService } from './user.service';
 import { ConnectUserDto } from './dto/connect-user.dto';
 import { User } from 'src/schemas/user.schema';
@@ -11,4 +11,16 @@ export class UserController {
   async createUser(@Body() connectUserDto: ConnectUserDto): Promise<User> {
     return await this.userService.createUser(connectUserDto);
   }
+
+  @Patch(':userId')
+  // modification des informations d'un utilisateur
+
+  @Delete(':userId')
+  // suppression d'un utilisateur
+
+  @Get()
+  // obtenir les informations de tous les utilisateurs pour l'admin
+
+  @Patch()
+  // modification du role admin d'un utilisateur par l'admin
 }
